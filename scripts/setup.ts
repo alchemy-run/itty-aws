f5ba8c822ce0e95592d339639baedc11180bbd89
Michael K <35264484+Mkassabov@users.noreply.github.com>
2025-12-30 10:42:39 -0500

    chore: setup script can be http based

--- scripts/setup.ts
+++ scripts/setup.ts
@@ -1,10 +1,27 @@
 import { $ } from "bun";
+import { parseArgs } from "util";
 
-const awsModels = Bun.file("aws-models")
-const exists = await awsModels.exists() || (await awsModels.stat().then(s => true).catch(() => false))
+const { values } = parseArgs({
+  args: Bun.argv.slice(2),
+  options: {
+    http: { type: "boolean", default: false },
+  },
+});
+
+const awsModels = Bun.file("aws-models");
+const exists =
+  (await awsModels.exists()) ||
+  (await awsModels
+    .stat()
+    .then(() => true)
+    .catch(() => false));
+
+const repoUrl = values.http
+  ? "https://github.com/aws/api-models-aws.git"
+  : "git@github.com:aws/api-models-aws.git";
 
 if (!exists) {
-  await $`git clone git@github.com:aws/api-models-aws.git aws-models --depth=1`
+  await $`git clone ${repoUrl} aws-models --depth=1`;
 } else {
-  await $`git pull --ff-only`.cwd("aws-models")
+  await $`git pull --ff-only`.cwd("aws-models");
 }
